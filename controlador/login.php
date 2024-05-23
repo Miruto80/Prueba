bfeb11da6bee69333243204df2f0eebfd30e1b2b
Miruto80 <miguelfernando8012@gmail.com>
2024-05-22 21:16:29 -0400

    Arreglando controlador

--- controlador/login.php
+++ controlador/login.php
@@ -1,9 +1,9 @@
 <?php 
-$nombre='miguel'
+
 if(is_file("vista/".$pagina.".php")){
     require_once("vista/".$pagina.".php");
 }else{
-    echo "la cagaste $nombre pa";
+    echo 'fallo el controlador login';
 }
 
 
