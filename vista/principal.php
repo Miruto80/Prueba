a2634b8011f60bfbb692206f84af1554839f66e7
Miruto80 <miguelfernando8012@gmail.com>
2024-05-24 10:48:52 -0400

    cambios

--- vista/principal.php
+++ vista/principal.php
@@ -1,4 +1,8 @@
 <html>
+    <head>
+        <title>Proyecto</title>
+        <link rel="shorcut icon" type="image/x-icon" href="img/logo taekyon.png">
+    </head>
 <?php require_once("comunes/framework.php"); ?>
 <body> 
 <?php require_once('comunes/nav.php'); ?>
