d426e4d378578ef43dd2f163d1373d3fc81f6d87
Miruto80 <miguelfernando8012@gmail.com>
2024-05-24 12:42:04 -0400

    correciones

--- vista/login.php
+++ vista/login.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <link rel="stylesheet" href="css/style_login.css">
-    <title>Login</title>
+    <title>Iniciar sesion</title>
 </head>
-
+<?php
+require_once("comunes/framewor.php");
+?>
 <body>
     <div class="container" id="container">
         <div class="form-container sign-up">
