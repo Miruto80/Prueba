80fe846db50b14b3407bc4dba51e8647ae0cdb28
Joacodm24 <joaquin_daniel234@outlook.com>
2024-05-24 12:24:20 -0400

    Nuevo login y el ..img

--- comunes/framework.php
+++ comunes/framework.php
@@ -5,4 +5,5 @@
  <script src="js/jquery.min.js" ></script>
  <script src="js/popper.min.js" ></script>
  <script src="js/bootstrap.bundle.js"></script>
+ <link rel="shortcut icon" type="image/x-icon" href="img/logo taekyon.png">
 </head>
--- vista/login.php
+++ vista/login.php
@@ -1,18 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shorcut icon" type="image/x-icon" href="img/logo taekyon.png">
-    <link rel="stylesheet" href="css/nav.css">
-    <title>Login</title>
+    <link rel="stylesheet" href="css/style-login.css">
+    <title>Iniciar sesión</title>
+    <link rel="shortcut icon" type="image/x-icon" href="img/logo taekyon.png">
 </head>
-<body>
-<h1>login</h1>
-<h1>login</h1>
-<h1>Prueba</h1>
-<a href="?pagina=principal">Hacia principal</a>
-</body>
-</html>
 
+<body>
+
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <button>Iniciar sesion</button>
+            </form>
+        </div>
+        <div class="form-container sign-in">
+            <form>
+                <h1>Iniciar sesión</h1>
+                <input type="email" placeholder="Email">
+                <input type="password" placeholder="Contraseña">
+                <button class="ingresar-b">Ingresar</button>
+            </form>
+        </div>
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>¡Bienvenido al sistema de la Taekyon!</h1>
+                    <p>Ingresa los respectivos datos para ingresar al sistema actual de nuestra academia.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
