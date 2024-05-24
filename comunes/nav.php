2340e5784016f9acc49cd4c36990364afba3ef73
Miruto80 <miguelfernando8012@gmail.com>
2024-05-24 11:43:49 -0400

    prueba

--- comunes/nav.php
+++ comunes/nav.php
@@ -1,40 +1,47 @@
-<header>
-    
-      <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark"">
-  <div class=" container-fluid">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/nav.css">
+  <title>TAEKYON</title>
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div class="container-fluid">
         <a class="navbar-brand" href=".">
           <img src="img/logo taekyon.png" alt="" width="40" height="auto" class="d-inline-block align-text-top">
-          <a class="navbar-brand" href="?pagina=principal"> TAEKYON </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href=".">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href=".">Registro</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Mas
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Accion 1</a></li>
-                  <li><a class="dropdown-item" href="#">Accion 2</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="#">Otra Accion 3</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-              <button class="btn btn-outline-warning" type="submit">Buscar</button>
-            </form>
-          </div>
+        </a>
+        <a class="navbar-brand" href="?pagina=principal"> TAEKYON </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href=".">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href=".">Registro</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Mas
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Accion 1</a></li>
+                <li><a class="dropdown-item" href="#">Accion 2</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Otra Accion 3</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+            <button class="btn btn-outline-warning" type="submit">Buscar</button>
+          </form>
+        </div>
+      </div>
     </nav>
-    </div>
   </header>
+</body>
+</html>
