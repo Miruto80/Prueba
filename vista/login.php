5708d0d96c40141730aa3ffbe2e3769b212b4ac2
Miruto80 <miguelfernando8012@gmail.com>
2024-05-24 12:45:50 -0400

    listo

--- vista/login.php
+++ vista/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style_login.css">
     <link rel="shorcut icon" type="image/x-icon" href="img/logo taekyon.png">
-    <title>iniciar sesion</title>
+    <title>Iniciar sesion</title>
 </head>
 
 <body>
