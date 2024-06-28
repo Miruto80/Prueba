c49aed46d02778739352d2bd90b6f7a5da8bdd18
elchelios56 <elchelios56@gmail.com>
2024-06-28 00:19:34 -0400

    1

--- vista/athletes.php
+++ vista/athletes.php
@@ -27,7 +27,7 @@
 			</div>
 					
 			<div class="col-md-1">	
-			    <a href="." class="btn btn-warning">Volver</a>
+			    <a href="." class="btn btn-warning">Salir</a>
 			</div>
 		</div>
 	</div>
