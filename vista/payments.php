2bedd749739f25497e8f00fa6457d85553cc69c9
elchelios56 <elchelios56@gmail.com>
2024-06-28 03:31:45 -0400

    1

--- vista/payments.php
+++ vista/payments.php
@@ -1,13 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+  
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  
 </head>
+<?php require_once("comunes/framework.php"); ?>
 <body>
-  <h1>Pagos</h1>
+
+<?php require_once('comunes/nav.php'); ?>
+
+<style>  
+		body {
+			background-image: url('img/fondo.jpg');
+          background-size: cover;
+          background-repeat: no-repeat;
+          
+		}
+    .blur {
+  filter: blur(10px); /* Puedes ajustar el valor para cambiar el nivel de desenfoque */
+}
+
+		.tablita {
+			color: white;
+		}
+
+    .floating-btn {
+      position: fixed;
+      bottom: 20px;
+      margin: 1px;
+    }
+	</style>
+
+
+<div class="container mt-3 text-center h2 text-warning">Gestion de Pagos<hr/>
+ 
+<img src="img/fondo.jpg" class="img-fluid blur">
+
+</div>
+<!--  
+<div class="tablita container text-center">
+	   <div class="table-responsive">
+		<table class="table table-striped table-dark table-hover" id="tablapago">
+			<thead>
+			  <tr>
+				<th>Monto</th>
+				<th>Cedula</th>
+				<th>fecha de pago</th>
+				<th>Comprobante de Pago</th>
+			  </tr>
+			</thead>
+			<tbody id="resultadoconsulta">
+			  
+			  
+			</tbody>
+	   </table>
+	  </div>
+  </div>
+-->
+<div class="container text-center">
+<div class="fixed-bottom">
+  <button type="button" class="btn btn-warning mr-4 mb-4">Agregar Pago</button>
+  <button type="button" class="btn btn-warning mr-4 mb-4">Listar Pagos</button>
+  <button type="button" class="btn btn-warning  mb-4">Salir</button>
+</div>
+  </div>
+ </div>
+ 
+
+
 </body>
+
 </html>
 
