20bf750eb18c4ea04d9766b086ef16e5a129c89e
Joacodm24 <joaquin_daniel234@outlook.com>
2024-07-09 19:24:04 -0400

    Nuevabd

--- Taekyon.sql
+++ Taekyon.sql
@@ -12,20 +12,13 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
---
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `personas`
+-- Estructura de tabla para la tabla `tatletas`
 --
 
 CREATE TABLE `tatletas` (
@@ -41,26 +34,20 @@ CREATE TABLE `tatletas` (
   `Telefono` varchar(30) NOT NULL,
   `Numerodeaccion` varchar(30) NOT NULL,
   `Cinturon` varchar(30) NOT NULL
-
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `personas`
+-- Volcado de datos para la tabla `tatletas`
 --
 
 INSERT INTO `tatletas` (`id`, `cedula`, `nombres`, `apellidos`, `fechadenacimiento`, `sexo`, `Participacion`,`Direccion`, `Correo`,`Telefono`,`Numerodeaccion`,`Cinturon`) VALUES
 (1, '30231853', 'Miguel Fernando', 'Torres Torres', '2004-08-10', 'M', 'Si','La mata','Mftt@gmail.com','04125889171','95','Blanco'),
 (2, '29418245', 'Daisa', 'Perez', '1976-09-14', 'F', 'Si','Cabudare','dail@gmail.com','04242580141','36','Rojo');
 
-
-
 --
 -- Índices para tablas volcadas
 --
 
---
--- Indices de la tabla `personas`
---
 ALTER TABLE `tatletas`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `cedula` (`cedula`);
@@ -69,9 +56,6 @@ ALTER TABLE `tatletas`
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
---
--- AUTO_INCREMENT de la tabla `personas`
---
 ALTER TABLE `tatletas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
@@ -80,18 +64,15 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
-
 CREATE TABLE `tpagos`(
-    `cedula`int(15),
+    `cedula` int(15),
     `fechadepago` date NOT NULL,
-    `Monto` float NOT Null,
+    `Monto` float NOT NULL,
     `Comprobantedepago` int (11) PRIMARY KEY
-    )ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `tpagos` (`Comprobantedepago`, `Monto`,`fechadepago`,`cedula`)
-
 VALUES (22,12.2,'2024/02/24',24351625);
-
 
 CREATE TABLE `tentrenadores` (
   `id` int(11) NOT NULL,
@@ -101,7 +82,6 @@ CREATE TABLE `tentrenadores` (
   `Telefono` varchar(15) NOT NULL,
   `Jerarquiadecinturon` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
 
 INSERT INTO `tentrenadores` (`id`, `CedulaE`, `Apellido`, `Nombre`, `Telefono`, `Jerarquiadecinturon`) VALUES
 (1, '30560144', 'Mendoza', 'Joaquin', '0414-5446656', 'VIII DAN'),
@@ -169,23 +149,23 @@ CREATE TABLE tHorarios(
     Tipodehorario varchar(30) PRIMARY KEY, 
     id2 int, 
     CedulaE2 int,
-    Edad int not Null, 
-    FOREIGN KEY (id2) REFERENCES tatletas(id),
-    FOREIGN KEY (CedulaE2) REFERENCES tentrenadores(CedulaE)  
-    )ENGINE=INNODB;
+    Edad int NOT NULL,
+    FOREIGN KEY (id2) REFERENCES tatletas(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (CedulaE2) REFERENCES tentrenadores(CedulaE) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
-INSERT INTO `tHorarios` (`Tipodehorario`, `id2`, `CedulaE2` , `Edad`)  VALUES
+INSERT INTO `tHorarios` (`Tipodehorario`, `id2`, `CedulaE2` , `Edad`) VALUES
 ('Juvenil',1,'30560144',23);
 
 CREATE TABLE tEventos( 
     Codevento int PRIMARY KEY, 
     id3 int, 
-    NombreEvento varchar(30) not Null, 
-    Logroobtenido varchar(30) not Null, 
-    FOREIGN KEY (id3) REFERENCES tatletas(id) 
-    )ENGINE=INNODB;
+    NombreEvento varchar(30) NOT NULL, 
+    Logroobtenido varchar(30) NOT NULL, 
+    FOREIGN KEY (id3) REFERENCES tatletas(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
 
-INSERT INTO `tEventos` (`Codevento`, `id3`, `NombreEvento`,`Logroobtenido`)  VALUES
+INSERT INTO `tEventos` (`Codevento`, `id3`, `NombreEvento`,`Logroobtenido`) VALUES
 (1,1,'Torneo Marcial','1er lugar');
 
-/*Insert into tEventos values(1,1,'Torneo Marcial','1er lugar');*/;
+COMMIT;
