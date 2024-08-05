33c27dcdcd9a86d0f0b9f5175a312929806449e0
Miruto80 <miguelfernando8012@gmail.com>
2024-08-05 11:48:40 -0400

    Cambios en usuarios

--- vista/Users.php
+++ vista/Users.php
@@ -81,7 +81,7 @@
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 				<div class="modal-header bg-dark text-warning" data-bs-theme="dark">
-					<h5 class="modal-title">Formulario de Entrenadores</h5>
+					<h5 class="modal-title">Formulario de Usuarios</h5>
 					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
 				<div class="modal-body">
