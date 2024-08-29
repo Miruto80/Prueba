48b59e1a515e5ad1f3b4f3c3a39911d114d77f4c
Miruto80 <miguelfernando8012@gmail.com>
2024-08-29 17:25:39 -0400

    Arreglando

--- comunes/nav.php
+++ comunes/nav.php
@@ -33,12 +33,12 @@
            
             <li class="nav-item dropdown ">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Gestionar
+                Gestionar datos
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="?pagina=schedules">Horarios</a></li>
-                <li><a class="dropdown-item" href="?pagina=payments">Pagos</a></li>
-                <li><a class="dropdown-item" href="?pagina=records">Logros</a></li>
+                <li><a class="dropdown-item" href="?pagina=schedules">Gestionar Horarios</a></li>
+                <li><a class="dropdown-item" href="?pagina=payments">Gestionar Pagos</a></li>
+                <li><a class="dropdown-item" href="?pagina=records">Gestionar Logros</a></li>
               </ul>
             </li>
 
