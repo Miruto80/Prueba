b1b59bc284a6e2c4316129a4f79f116332926482
Joacodm24 <joaquin_daniel234@outlook.com>
2024-10-07 16:01:05 -0400

    nav nuevo

--- comunes/nav.php
+++ comunes/nav.php
@@ -3,12 +3,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TAEKYON</title>
 </head>
+
 <body>
   <header>
     <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="?pagina=principal">
-          <img src="img/logo taekyon.png" alt="" width="45" height="auto" class="d-inline-block align-text-top">
+          <img src="img/logo taekyon.png" alt="Logo Taekyon" width="45" height="auto" class="d-inline-block align-text-top">
         </a>
         <a class="navbar-brand" href="?pagina=principal"> TAEKYON </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,19 +20,18 @@
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="?pagina=principal">Inicio</a>
             </li>
-             
-            <li class="nav-item dropdown ">
+
+            <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Registrar datos
               </a>
               <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="?pagina=athletes">Inscribir Atletas</a></li>
                 <li><a class="dropdown-item" href="?pagina=trainers">Incluir Entrenadores</a></li>
-                
               </ul>
             </li>
-           
-            <li class="nav-item dropdown ">
+
+            <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Gestionar datos
               </a>
@@ -41,32 +41,32 @@
                 <li><a class="dropdown-item" href="?pagina=records">Gestionar Logros</a></li>
               </ul>
             </li>
+          </ul>
 
+          <!-- Botones alineados a la derecha -->
+          <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link" href="?pagina=Users">Usuarios</a>
+              <a class="nav-link" href="?pagina=Users">
+              <img src="img/iconob.png" alt="Icono Usuario" width="30" height="30" class="d-inline-block align-text-top">
+              </a>
             </li>
 
             <li class="nav-item">
-            <?php
-		        
-            if(!empty($nivel) and $nivel!=""){
-         ?>
-              <a href="?pagina=salida" class="btn btn-outline-warning my-2 my-sm-0">Salir</a>
-         <?php	
-            }
-            ?>
+              <?php
+              if (isset($nivel) && !empty($nivel)) {
+              ?>
+                <a href="?pagina=salida" class="btn btn-outline-light my-2 my-sm-0">
+                <img src="img/salirb.svg" alt="Icono Salir" width="30" height="auto" class="d-inline-block align-text-top">
+                </a>
+              <?php
+              }
+              ?>
             </li>
           </ul>
-          
-          
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" aria-label="Search">
-            <button class="btn btn-outline-warning" type="submit">Buscar</button>
-          </form>
         </div>
       </div>
     </nav>
   </header>
-
 </body>
+
 </html>
